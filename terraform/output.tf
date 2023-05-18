02ac8d40b18995b3f4ad1024091cdcde3f6d51db
Varun Sharma <varunvgs2000@gmail.com>
2023-05-18 21:08:00 +0530

    testing environmentalization

--- terraform/output.tf
+++ terraform/output.tf
@@ -2,7 +2,7 @@ output "public_dns_address" {
   value = aws_instance.ec2_instance.public_ip
 }
 
-resource "local_file" "public_ip" {
-    content  = venafi_certificate.this.private_key_pem
-    filename = "private_key.pem"
-}
+# resource "local_file" "public_ip" {
+#     content  = venafi_certificate.this.private_key_pem
+#     filename = "private_key.pem"
+# }
