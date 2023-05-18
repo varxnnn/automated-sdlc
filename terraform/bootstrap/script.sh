76c4c5a4b0a489921cb1d2ed5cba8adaf97ff1f6
Varun Sharma <varunvgs2000@gmail.com>
2023-05-18 22:12:22 +0530

    added aws config again

--- terraform/bootstrap/script.sh
+++ terraform/bootstrap/script.sh
@@ -2,5 +2,3 @@
 sudo yum -y install docker
 sudo usermod -a -G docker ec2-user
 sudo systemctl start docker
-docker pull varxn/major-react:0.2
-docker run -d --name react --restart unless-stopped -p 80:80 varxn/major-react:0.2
